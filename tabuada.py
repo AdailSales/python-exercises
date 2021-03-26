2bd25939093a849fe950e4ec2d51d02bfe03e7d1
Adail Sales <65291508+AdailSales@users.noreply.github.com>
2021-03-26 11:17:15 -0300

    Add exercise: Tabuada de multiplicar.
    Foi adicionado o arquivo tabuada.py, este exercício corresponde a uma tabuada de multiplicação.

---
+++ tabuada.py
@@ -0,0 +1,20 @@
+#!usr/bin/python
+
+number = int(input('Digite um número para ver sua tabuada de multiplicação: '))
+
+print (
+    '---------------------------'
+    f'\nTabuada do {number}.\n'
+    '---------------------------'
+    f'\n{number} x {1:2} = {number * 1}\n'
+    f'{number} x {2:2} = {number * 2}\n'
+    f'{number} x {3:2} = {number * 3}\n'
+    f'{number} x {4:2} = {number * 4}\n'
+    f'{number} x {5:2} = {number * 5}\n'
+    f'{number} x {6:2} = {number * 6}\n'
+    f'{number} x {7:2} = {number * 7}\n'
+    f'{number} x {8:2} = {number * 8}\n'
+    f'{number} x {9:2} = {number * 9}\n'
+    f'{number} x {10} = {number * 10}\n'
+    '---------------------------'       )
+    
