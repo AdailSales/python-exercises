8254fe6e704881608e309508e7f385bb9b924a18
Adail Sales <65291508+AdailSales@users.noreply.github.com>
2021-03-28 18:33:34 -0300

    Add exercise: imc
    Foi adicionado o arquivo imc.py, referente ao exercício para calcular o valor do IMC a partir dos dados informados pelo usuário.

---
+++ imc.py
@@ -0,0 +1,7 @@
+peso = float(input("Qual é o seu peso? "))
+altura = float(input("Qual é a sua altura? "))
+
+IMC = peso / (altura ** 2)
+
+print (f"O seu IMC (índice de massa corporal) é {IMC:.2f}.")
+
