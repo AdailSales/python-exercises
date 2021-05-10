08b21f3016cb7cab3fe910cdb63d0e3105908ea4
Adail Sales <65291508+AdailSales@users.noreply.github.com>
2021-05-10 06:59:18 -0300

    Update
    Foram feitas alteraçãoes nas linhas 2, 8 e 9 do script adicionando o módulo date da biblioteca datetime e um novo bloco de if. Caso, o usuário dê como entrada o valor 0, será analisado se o ano atual é bissexto ou não.

--- ano_bissexto.py
+++ ano_bissexto.py
@@ -1,12 +1,14 @@
 # Faça um programa que leia um ano qualquer e mostre se ele é bissexto.
+from datetime import date
 import calendar
 
 year = int(input('Digite o ano: '))
 
 leap_year = calendar.isleap(year)
 
+if year == 0:
+    year = date.today().year
 if leap_year is True:
     print(f'O ano de {year} é bissexto.')
 else:
     print(f'O ano de {year} não é bissexto.')
-
